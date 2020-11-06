f33e07e674c7c4e87470d2ea3fbc43ebe87a7a4b
TAN.TON <nhattan2007@gmail.com>
2020-11-06 11:52:42 +0700

    C2 - [Thuc hanh] Su dung toan tu

---
+++ myscripts.js
@@ -0,0 +1,5 @@
+let inputWidth = parseFloat(prompt("Nhập chiều dài:"));
+let inputHeight = parseFloat(prompt("Nhập chiều rộng:"));
+let area = inputHeight * inputWidth;
+
+document.write("Diện tích hình chữ nhật là: " + area);
