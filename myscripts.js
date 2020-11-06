add1bf2a3fb0866d99b6e6769b480bed34e83b51
TAN.TON <nhattan2007@gmail.com>
2020-11-06 14:25:17 +0700

    C3 - [Bai tap] Bien, kieu du lieu va toan tu

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,20 @@
-let inputWidth = parseFloat(prompt("Nhập chiều dài:"));
-let inputHeight = parseFloat(prompt("Nhập chiều rộng:"));
-let area = inputHeight * inputWidth;
+var scoreMath= document.getElementById(id="scoreMath").innerHTML = prompt("Nhập điểm Toán:");
+while (parseFloat(scoreMath) > 10 || parseFloat(scoreMath) < 0) {
+      scoreMath= document.getElementById(id="scoreMath").innerHTML = prompt("Nhập điểm Toán:")
+}
+var scorePhysics = document.getElementById(id="scorePhysics").innerHTML = prompt("Nhập điểm Lý:");
+while (parseFloat(scorePhysics) > 10 || parseFloat(scorePhysics) < 0) {
+      scorePhysics = document.getElementById(id="scorePhysics").innerHTML = prompt("Nhập điểm Lý:");
+}
+var scoreChemistry = document.getElementById(id="scoreChemistry").innerHTML = prompt("Nhập điểm Hóa:");
+while (parseFloat(scoreChemistry) > 10 || parseFloat(scoreChemistry) < 0) {
+      scoreChemistry = document.getElementById(id="scoreChemistry").innerHTML = prompt("Nhập điểm Hóa:");
+}
+scoreMath = parseFloat(parseFloat(scoreMath).toFixed(1));
+scorePhysics = parseFloat(parseFloat(scorePhysics).toFixed(1));
+scoreChemistry = parseFloat(parseFloat(scoreChemistry).toFixed(1));
+document.getElementById("calculator").addEventListener("click",caculatorAverageScore);
 
-document.write("Diện tích hình chữ nhật là: " + area);
+function caculatorAverageScore(){
+      document.getElementById(id="averageScore").innerHTML = ((scoreMath + scorePhysics + scoreChemistry)/3).toFixed(1);
+}
